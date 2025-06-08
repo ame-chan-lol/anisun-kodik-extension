436ee574bbe8bca35bece7c0011986ce278ee751
notwindstone <notwindstone@gmail.com>
2025-06-08 21:34:34 +0300

    perf!: reduce bundle size from 255 kbs to 43 kbs

--- remote-component.config.js
+++ remote-component.config.js
@@ -5,5 +5,7 @@
 module.exports = {
     resolve: {
       react: require("react"),
+      "react-dom/client": require("react-dom/client"),
+      "@tanstack/react-query": require("@tanstack/react-query"),
     },
 };
